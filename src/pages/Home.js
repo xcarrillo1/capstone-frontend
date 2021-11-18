5d489666801dcc8058c71cdf6d4fa9f5f52b7759
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-11-18 16:58:18 -0500

    giphy on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 export default function Home(props) {
     return (
         <div className="home">
-            <h1>Home Page</h1>
+            <br/>
+            <img src="https://media.giphy.com/media/8OSShUr7ebVRwtCaq2/giphy.gif" alt="Bad-View" /> 
         </div>
     )
 } 
