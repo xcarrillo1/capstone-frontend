fc2a06572826647a36f2832b396d05a418fd3ce3
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-11-18 23:15:44 -0500

    loading shows up on allpost page

--- src/pages/AllPost.js
+++ src/pages/AllPost.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom"
+
 export default function AllPost(props) {
-    return (
-        <div>
-            <h1>AllPost Page</h1>
-        </div>
-    )
+    const loaded = () => {
+        return props.post.map((post) => (
+            <div key={post._id} className="post">
+                <Link to={`/allpost/${post._id}`}><img src={post.image_url} alt={post.slocation} /></Link>
+            </div>
+        ));
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    };
+
+    return props.post ? loaded() : loading()
 } 
