af21130dab27f154c6ed7c093ab6aa3ffdea125b
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-11-18 23:37:58 -0500

    have pictures show on the allpost page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from 'react-router-dom';
 import AllPost from '../pages/AllPost';
 import CreatePost from '../pages/CreatePost';
@@ -6,12 +7,35 @@ import Show from '../pages/Show';
 import Vt from '../pages/Vt';
 
 export default function Main(props) {
+    const [post, setPost] = useState(null);
+
+    const URL = "https://lit-bastion-80745.herokuapp.com/seats/";
+
+    const getPost = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setPost(data);
+    };
+
+    const createPost = async (post) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(post),
+        });
+        getPost();
+    };
+
+    useEffect(() => getPost(), []);
+
     return (
         <main>
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/allpost' element={<AllPost />} />
-                <Route path='/createpost' element={<CreatePost />} />
+                <Route path='/allpost' element={<AllPost post={post} />} />
+                <Route path='/createpost' element={<CreatePost post={post} createPost={createPost} />} />
                 <Route path='/allpost/:id' element={<Show />} />
                 <Route path='/vt' element={<Vt />} />
             </Routes>
